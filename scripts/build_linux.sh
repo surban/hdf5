b780050eee5a242bcf283353709bb56c5e488914
Sebastian Urban <surban@surban.net>
2018-05-07 14:11:29 +0200

    build scripts

---
+++ scripts/build_linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $(dirname "${BASH_SOURCE[0]}")
+cd ..
+
+mkdir -p build_linux
+cd build_linux
+cmake -DHDF5_BUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DHDF5_BUILD_CPP_LIB:BOOL=OFF -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON  ..
+make -j
+cd ..
+
